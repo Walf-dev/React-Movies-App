6770121e2097a876e0663afeca6a77c37765ecfd
Wagni Fredoly <starkwagni@gmail.com>
2021-02-26 07:15:55 +0100

    first commit
    react movie app entire app

---
+++ src/App.js
@@ -0,0 +1,145 @@
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
+import AddFavourites from './components/AddFavourites';
+import RemoveFavourites from './components/RemoveFavourites';
+
+const App = () => {
+	const [favourites, setFavourites] = useState([]);
+	const [searchValue, setSearchValue] = useState('');
+	const [movies, setMovies] = useState([
+		{
+			Title: 'Star Wars: Episode IV - A New Hope',
+			Year: '1977',
+			imdbID: 'tt0076759',
+			Type: 'movie',
+			Poster:
+				'https://images-na.ssl-images-amazon.com/images/I/71oTHnwkNDL._V1_SX300_.jpg',
+		},
+		{
+			Title: 'Star Wars: Episode V - The Empire Strikes Back',
+			Year: '1980',
+			imdbID: 'tt0080684',
+			Type: 'movie',
+			Poster:
+				'https://images-na.ssl-images-amazon.com/images/I/71-6ttsgzVL._V1_SX300_.jpg',
+		},
+		{
+			Title: 'Star Wars: Episode VI - Return of the Jedi',
+			Year: '1983',
+			imdbID: 'tt0086190',
+			Type: 'movie',
+			Poster:
+			'https://images-na.ssl-images-amazon.com/images/I/819fsS-IpoL._V1_SX300_.jpg',
+		},
+		{
+			Title: 'Star Wars: Episode VI - Return of the Jedi',
+			Year: '1983',
+			imdbID: 'tt0086190',
+			Type: 'movie',
+			Poster:
+				'https://images-na.ssl-images-amazon.com/images/I/81aY-ypFDaL._V1_SX300_.jpg',
+		},
+		{
+			Title: 'Star Wars: Episode VI - Return of the Jedi',
+			Year: '1983',
+			imdbID: 'tt0086190',
+			Type: 'movie',
+			Poster:
+			'https://images-na.ssl-images-amazon.com/images/I/91Cb914I1UL._V1_SX300_.jpg',
+		},
+		{
+			Title: 'Star Wars: Episode VI - Return of the Jedi',
+			Year: '1983',
+			imdbID: 'tt0086190',
+			Type: 'movie',
+			Poster:
+				'https://images-na.ssl-images-amazon.com/images/I/61MCwtM0LaL._V1_SX300_.jpg',
+		},
+		{
+			Title: 'Star Wars: Episode VI - Return of the Jedi',
+			Year: '1983',
+			imdbID: 'tt0086190',
+			Type: 'movie',
+			Poster:
+				'https://images-na.ssl-images-amazon.com/images/I/71%2BxRfP7HfL._V1_SX300_.jpg',
+		},
+
+
+	]);
+
+	const getMovieRequest = async (searchValue) => {
+		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=ba5bdde5`;
+
+		const response = await fetch(url);
+		const responseJson = await response.json();
+
+		if (responseJson.Search) {
+			setMovies(responseJson.Search);
+		}
+	};
+
+	useEffect(() => {
+		getMovieRequest(searchValue);
+	}, [searchValue]);
+
+	useEffect(() => {
+		const movieFavourites = JSON.parse(
+			localStorage.getItem('react-movie-app-favourites')
+		);
+
+		if (movieFavourites) {
+			setFavourites(movieFavourites);
+		}
+	}, []);
+
+	const saveToLocalStorage = (items) => {
+		localStorage.setItem('react-movie-app-favourites', JSON.stringify(items));
+	};
+
+	const addFavouriteMovie = (movie) => {
+		const newFavouriteList = [...favourites, movie];
+		setFavourites(newFavouriteList);
+		saveToLocalStorage(newFavouriteList);
+	};
+
+	const removeFavouriteMovie = (movie) => {
+		const newFavouriteList = favourites.filter(
+			(favourite) => favourite.imdbID !== movie.imdbID
+		);
+
+		setFavourites(newFavouriteList);
+		saveToLocalStorage(newFavouriteList);
+	};
+
+	return (
+		<div className='container-fluid movie-app'>
+			<div className='row d-flex align-items-center mt-4 mb-4'>
+				<MovieListHeading heading='React' />
+				<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+			</div>
+			<div className='row'>
+				<MovieList
+					movies={movies}
+					handleFavouritesClick={addFavouriteMovie}
+					favouriteComponent={AddFavourites}
+				/>
+			</div>
+			<div className='row d-flex align-items-center mt-4 mb-4'>
+				<MovieListHeading heading='Loved' />
+			</div>
+			<div className='row'>
+				<MovieList
+					movies={favourites}
+					handleFavouritesClick={removeFavouriteMovie}
+					favouriteComponent={RemoveFavourites}
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default App;
---
+++ src/components/MovieListHeading.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const MovieListHeading = (props) => {
+	return (
+		<div className='col'>
+			<h1>
+                {props.heading}
+                <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="azure" class="bi bi-camera-reels-fill" viewBox="0 0 16 16">
+                    <path d="M6 3a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                    <path d="M9 6a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
+                    <path d="M9 6h.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 7.269v7.462a1 1 0 0 1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 16H2a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h7z"/>
+                </svg>
+            </h1>
+		</div>
+	);
+};
+
+export default MovieListHeading;
