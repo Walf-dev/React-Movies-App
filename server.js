1e5a40936659163e5d638414a1814a379b86d37a
Wagni Fredoly <starkwagni@gmail.com>
2021-02-27 15:33:35 +0100

    API test

--- server.js
+++ server.js
@@ -1,21 +1,12 @@
-const express = require('express')
-const path = require('path')
-const bodyParser = require('body-parser')
-
-const app = express()
-
-const port = process.env.PORT || 3000
-
-app.use(express.static(path.join(__dirname, 'public')))
-
-// middleware to handle JSON parsing
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'))
-})
-
+const path = require('path');
+const express = require('express');
+const app = express();
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
+app.use(express.static(publicPath));
+app.get('*', (req, res) => {
+   res.sendFile(path.join(publicPath, 'index.html'));
+});
 app.listen(port, () => {
-  console.log(`Server running on port => ${port}`)
-})
+   console.log('Server is up!');
+});
